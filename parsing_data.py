86dfb76b303763da2fbdfee65c42c8bd804ac3fe
YoungsunCho <cyssun@kookmin.ac.kr>
2019-03-14 20:55:46 +0900

    Upload final assignment result

--- parsing_data.py
+++ parsing_data.py
@@ -1,5 +1,6 @@
-import os
 import csv
+from datetime import datetime
+import os
 
 def open_csv_file():
     csv_file = open('data.csv', 'w', encoding='utf-8', newline='')
@@ -9,7 +10,7 @@ def open_csv_file():
 def close_csv_file(csv_file):
     csv_file.close()
 
-def parsing(filename,wr):
+def parsing(filename,wr,header):
     file = open(filename, 'r')
     dic = {}
 
@@ -30,23 +31,71 @@ def parsing(filename,wr):
             # value parsing
             value = line.replace(key, "")[:-1]
             value = value[1:].lstrip()
-
             # print(value)
             dic[key] = value
 
-    print(dic.items())
-    wr.writerow(dic.items())
+    list = []
 
+    for head in header:
+        if head == "To" or head == "Cc" or head == "Bcc":
+            continue
+
+        if dic.get(head) != None:
+            if head == "Date":
+                t = datetime.strptime(dic.get(head)[:-6],
+                                      '%a, %d %b %Y %H:%M:%S %z')
+                list.append(t.strftime("%Y-%m-%d %H:%M:%S"))
+            else:
+                list.append(dic.get(head))
+        else:
+            list.append("")
+
+    for head in header:
+        if head == "To" or head == "Cc" or head == "Bcc":
+            content = dic.get(head)
+            if content != None:
+                if content.find(',') == None:
+                    newlist = list
+                    con = content.strip()
+                    newlist.insert(0, head)
+                    newlist.insert(0, content)
+                    wr.writerow(newlist)
+                    del newlist[0]
+                    del newlist[0]
+                else :
+                    contentlist = content.split(',')
+                    for to in contentlist:
+                        newlist = list
+                        to = to.strip()
+                        newlist.insert(0, head)
+                        newlist.insert(0, to)
+                        wr.writerow(newlist)
+                        del newlist[0]
+                        del newlist[0]
     file.close()
 
-def search(dirname,wr):
+def search(dirname,wr,header):
     filenames = os.listdir(dirname)
     for filename in filenames:
         full_filename = os.path.join(dirname, filename)
         # print (full_filename)
-        wr.writerow([filename])
-        parsing(full_filename,wr)
+        # wr.writerow([filename])
+        parsing(full_filename,wr,header)
 
-csv_file, wr = open_csv_file()
-search("c:/enron_email/emails/",wr)
-close_csv_file(csv_file)
+if __name__ == "__main__":
+
+    csv_file, wr = open_csv_file()
+
+    header = ["Message-ID", "Date", "From", "To", "Subject", "Cc",
+              "Mime-Version", "Content-Type", "Content-Transfer-Encoding",
+              "Bcc", "X-From", "X-To", "X-cc", "X-bcc", "X-Folder",
+              "X-Origin", "X-FileName"]
+
+    newheader = ["Recipient", "Recipient-type", "Message-ID", "Date", "From",
+                 "Subject", "Mime-Version", "Content-Type",
+                 "Content-Transfer-Encoding", "X-From", "X-To", "X-cc",
+                 "X-bcc", "X-Folder", "X-Origin", "X-FileName"]
+
+    wr.writerow(newheader)
+    search("c:/enron_email/emails/",wr, header)
+    close_csv_file(csv_file)
